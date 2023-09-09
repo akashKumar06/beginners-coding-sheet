eb7fe790ac92489d881fd98ac1334d38803913a5
Akash Kumar <akashkumar59999@gmail.com>
2023-09-09 12:59:40 +0530

    day 1

---
+++ 5/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"\n\t\tSizes of data types in my system\n\n";
+    cout<<"int takes: "<<sizeof(int)<<endl;
+    cout<<"float takes: "<<sizeof(float)<<endl;
+    cout<<"double takes: "<<sizeof(double)<<endl;
+    cout<<"char takes: "<<sizeof(char)<<endl;
+    return 0;
+}
